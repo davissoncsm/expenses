19783cc2c35da1374ab2b5ccfa8aa2d556b05942
davissoncsm <davisson.charles@gmail.com>
2024-04-05 16:56:02 -0300

    Change User model to User entity

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,10 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\UserEntity::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
+        // \App\Models\UserEntity::factory()->create([
+        //     'name' => 'Test UserEntity',
         //     'email' => 'test@example.com',
         // ]);
     }

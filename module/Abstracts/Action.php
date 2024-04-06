bf0dd0efc711825f00fbb0a386a7fa6d215a3246
davissoncsm <davisson.charles@gmail.com>
2024-04-06 10:50:01 -0300

    Change abstract namespace to Action and Service classes

--- module/Abstracts/Action.php
+++ module/Abstracts/Action.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Module\User\Actions;
+namespace Module\Abstracts;
 
 abstract class Action
 {
--- module/Abstracts/Service.php
+++ module/Abstracts/Service.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Module\User\Services;
+namespace Module\Abstracts;
 
 abstract class Service
 {

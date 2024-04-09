6e9759a2646a23c27299a066522043d302b0dd7c
davissoncsm <davisson.charles@gmail.com>
2024-04-09 20:58:18 -0300

    Add names to routes

--- routes/api.php
+++ routes/api.php
@@ -6,22 +6,22 @@ use Module\Card\Handlers\expense\CreateExpenseHandler;
 use Module\User\Handlers\{CreateUserHandler, DeleteUserHandler, LoginHandler, UpdateUserHandler};
 
 Route::group(['excluded_middleware' => 'auth:sanctum'], function (){
-    Route::post('/login', LoginHandler::class);
+    Route::post('/login', LoginHandler::class)->name('login');
 });
 
 Route::group(['prefix' => 'users'], function (){
-    Route::post('/', CreateUserHandler::class);
-    Route::put('/{id}', UpdateUserHandler::class);
-    Route::delete('/{id}', DeleteUserHandler::class);
+    Route::post('/', CreateUserHandler::class)->name('users.store');
+    Route::put('/{id}', UpdateUserHandler::class)->name('users.update');
+    Route::delete('/{id}', DeleteUserHandler::class)->name('users.delete');
 });
 
 Route::group(['prefix' => 'cards'], function (){
-    Route::get('/', GetCardsHandler::class);
-    Route::post('/', CreateCardHandler::class);
-    Route::put('/{id}', UpdateCardHandler::class);
-    Route::delete('/{id}', DeleteCardHandler::class);
+    Route::get('/', GetCardsHandler::class)->name('cards.index');
+    Route::post('/', CreateCardHandler::class)->name('cards.store');
+    Route::put('/{id}', UpdateCardHandler::class)->name('cards.update');
+    Route::delete('/{id}', DeleteCardHandler::class)->name('cards.delete');
 });
 
 Route::group(['prefix' => 'expenses'], function (){
-    Route::post('/', CreateExpenseHandler::class);
+    Route::post('/', CreateExpenseHandler::class)->name('expenses.store');
 });
